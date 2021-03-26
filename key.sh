5e1eecd976ff84050705f21529defcad7c48b62e
vjayv <v95jay@gmail.com>
2021-03-26 12:48:26 +0530

    updated UI and basic features

---
+++ key.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true; do
+  xdotool key leftarrow
+  sleep 2
+  xdotool key rightarrow
+  return
+done
--- sounds.js
+++ sounds.js
@@ -1,4 +1,4 @@
-var keyData = {
+export let keyData = {
 
     q: {
         sound: new Howl({
